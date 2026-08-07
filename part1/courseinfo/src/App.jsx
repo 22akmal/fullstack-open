772028a3948d6bfb0c8036d804ab926317b58eab
22akmal <akmalsyam67@gmail.com>
2026-08-07 14:47:45 +0700

    Finished exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,16 +1,25 @@
 const Header = (props) => {
   console.log(props)
   return (
-    <>
     <h1>{props.course}</h1>
-    </>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>{props.part} {props.exercises}</p>
   )
 }
 
 const Content = (props) => {
   console.log(props)
   return (
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <Part part={props.part[0]} exercises={props.exercises[0]}/>
+      <Part part={props.part[1]} exercises={props.exercises[1]}/>
+      <Part part={props.part[2]} exercises={props.exercises[2]}/>
+    </div>
   )
 }
 
@@ -33,9 +42,7 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]}/>
       <Total sum={exercises1 + exercises2 + exercises3}/>
     </div>
   )
